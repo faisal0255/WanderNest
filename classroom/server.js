80620746442321cc704eb2bb611444419fc80fb0
Faisal Raza <faisalraza841311@gmail.com>
2026-04-26 15:55:27 +0530

    using connect-flash to display flash msgs

--- classroom/server.js
+++ classroom/server.js
@@ -3,21 +3,48 @@ const app = express();
 const users = require("./routes-2/user.js");
 const posts = require("./routes-2/post.js");
 const session = require("express-session");
+const flash =  require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const sessionOptions = {
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: true,
+};
 
 app.get("/", () =>{
         res.send("Hii i am root route");
 });
 
-app.use(session({
-        secret: "keyboard cat",
-        resave: false,
-        saveUninitialized: true,
-}));
+app.use(session(sessionOptions));
+app.use(flash());
 
 app.use("/users", users);
 app.use("/posts", posts);
 
+app.get("/register", (req, res) => {
+    let { name = "anonymous"} = req.query;
+    req.session.name = name;
+    req.flash("success", "User registered succesfully")
+    res.redirect("/hello");
+})
 
+app.get("/hello", (req, res) => {
+
+    res.render("page.ejs", {name: req.session.name, msg: req.flash("success")});
+})
+
+app.get("/reqcount", (req, res) => {
+    if(req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1
+    }
+    res.send(`You send a request ${req.session.count} times`);
+})
 
 app.get("/test", (req, res) => {
     res.send("Test successful");
