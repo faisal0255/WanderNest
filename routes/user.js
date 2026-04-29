96dbd4017fe987ba38e7441ed823a5d645b7f6aa
Faisal Raza <faisalraza841311@gmail.com>
2026-04-29 17:16:39 +0530

    implementing signin functionality

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router =  express.Router();
-const User = require("../models/user.js")
+const User = require("../models/user.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs")
@@ -18,6 +19,17 @@ router.post("/signup", async(req,res) => {
     req.flash("error", e.message);
     res.redirect("/signup");
    }
+});
+
+// login the Users
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs")
+});
+
+router.post("/login", passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}), async(req,res) =>{
+    req.flash("success", "Welcome Back to World of WonderNest");
+    res.redirect("/listings")
 })
 
 module.exports = router;
