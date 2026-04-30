5fe66eef16022baba6092ccec7f6a5ec49818255
Faisal Raza <faisalraza841311@gmail.com>
2026-05-01 01:42:54 +0530

    adding post login pages

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router =  express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
+const {saveRedirectUrl } = require("../middleware.js")
+
+// Sign Up the user
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs")
@@ -13,8 +16,14 @@ router.post("/signup", async(req,res) => {
         const newUser = new User({username, email});
         const registeredUser = await User.register({email, username}, password);
         console.log(registeredUser);
-        req.flash("success", "Welcome to WanderNest");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err);
+            }
+                req.flash("success", "Welcome to WanderNest");
+                res.redirect("/listings");
+        })
+       
    } catch(e) {
     req.flash("error", e.message);
     res.redirect("/signup");
@@ -27,9 +36,26 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs")
 });
 
-router.post("/login", passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}), async(req,res) =>{
+router.post("/login", 
+    saveRedirectUrl,
+    passport.authenticate("local", 
+    {failureRedirect: "/login",
+    failureFlash: true}), async(req,res) =>{
     req.flash("success", "Welcome Back to the World of WonderNest");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+});
+
+// Logout The use
+
+router.get("/logout", (req, res, next) => {
+   req.logout((err) =>{
+    if(err) {
+        return next(err);
+    }
+    req.flash("success", "You are loggedOut");
     res.redirect("/listings")
-})
+   });
+});
 
 module.exports = router;
